aa60f97911298a6e4a5744f10efa26c95973feae
Jeffery Olson <olson.jeffery@gmail.com>
2009-03-02 21:04:53 -0800

    fixing some brokenness w/ namespaces and a messed up directory name

--- Source/Mercury.ExampleSite.Mvc.Cs/Global.asax.cs
+++ Source/Mercury.ExampleSite.Mvc.Cs/Global.asax.cs
@@ -11,7 +11,7 @@ namespace Mercury.ExampleSite
   // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
   // visit http://go.microsoft.com/?LinkId=9394801
 
-  public class MvcApplication : System.Web.HttpApplication
+  public class MercuryApplication : System.Web.HttpApplication
   {
     public static void RegisterRoutes(RouteCollection routes)
     {
