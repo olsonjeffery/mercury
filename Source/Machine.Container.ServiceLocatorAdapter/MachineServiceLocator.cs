2046b803f9719a85e8a5c2921cc98365f77560f1
Jeffery Olson <olson.jeffery@gmail.com>
2009-03-03 14:49:11 -0800

    adding an IServiceLocator impl for Machine.Container.. hopefully this goes away soon

---
+++ Source/Machine.Container.ServiceLocatorAdapter/MachineServiceLocator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.ServiceLocation;
+using Machine.Container;
+
+namespace Machine.Container.ServiceLocatorAdapter
+{
+  /// <summary>
+  /// Description of MyClass.
+  /// </summary>
+  public class MachineServiceLocator : ServiceLocatorImplBase
+  {
+    MachineContainer _container;
+    public MachineServiceLocator(MachineContainer container)
+    {
+      _container = container;
+    }
+    
+    public MachineServiceLocator()
+    {
+      _container = new MachineContainer();
+    }
+    
+    protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+    {
+      return _container.Resolve.All(serviceType);
+    }
+    
+    protected override object DoGetInstance(Type serviceType, string key)
+    {
+      return _container.Resolve.Named(key);
+    }
+  }
+}
