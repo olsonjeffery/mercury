e6ade172bcdfa7e40ac2d0801dfc4fa9c203b864
Jeffery Olson <olson.jeffery@gmail.com>
2009-03-04 16:24:44 -0800

    adding default.msbuild and rakefile.rb

---
+++ rakefile.rb
@@ -0,0 +1,32 @@
+#
+# $Id$
+#
+
+require 'rake'
+require 'rake/testtask'
+require 'pathname'
+require 'rubygems'
+gem 'activerecord', '>=1.15.3'
+require 'active_record'
+require 'fileutils'
+include FileUtils
+
+task :sln do
+  Thread.new do
+    system "devenv Source/Mercury.sln"
+  end
+end
+
+task :specs => "specs:view"
+
+namespace :specs do
+  desc "Run Server Specs"
+  task :run do
+    system "msbuild default.msbuild /t:RunSpecs"
+  end
+
+  desc "Run and View Server Specs"
+  task :view => :run do
+    system "start Specs.html"
+  end
+end
