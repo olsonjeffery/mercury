c7dc05a32843581567862876e58c92cc5ae25040
Jeffery Olson <olson.jeffery@gmail.com>
2009-03-17 08:05:26 -0700

    reworked global.asax.cs

--- Source/Mercury.ExampleSite.Mvc.Cs/Global.asax.cs
+++ Source/Mercury.ExampleSite.Mvc.Cs/Global.asax.cs
@@ -17,31 +17,23 @@ namespace Mercury.ExampleSite
   public class MercuryApplication : System.Web.HttpApplication
   {
     
-    public static IEnumerable<Route> ConfigureMercuryEngine(IServiceLocator container, ViewEngineCollection viewEngines)
-    {
-      //var view = viewEngines[0].FindView(self.ControllerContext, "Home.spark", "\\Views\\Layouts\\Application.spark", false);
-      
-      
-      var engine = new MercuryStartupService(container, viewEngines);
-      return engine.BuildRoutes();
-      /*routes.MapRoute(
-          "Default",                                              // Route name
-          "{controller}/{action}/{id}",                           // URL with parameters
-          new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-      );*/
-    }
-
-    protected void Application_Start()
+    public static IEnumerable<Route> ConfigureMercuryEngine()
     {
       var viewEngines = ConfigureViewEngines();
       var container = ConfigureContainer();
-      var routes = ConfigureMercuryEngine(container, viewEngines);
+      var engine = new MercuryStartupService(container, viewEngines);
+      var routes = engine.BuildRoutes();
       
       RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
       foreach(var route in routes) {
         RouteTable.Routes.Add(route.Url, route);
       }
-      
+      return routes;
+    }
+
+    protected void Application_Start()
+    {
+      ConfigureMercuryEngine();
     }
     
     protected static IServiceLocator ConfigureContainer() {
