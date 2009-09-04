2cba2207d7cbaf9e852807dd0be3c2fc90bfc99f
jeff <jeff@eee.(none)>
2009-09-04 10:17:06 -0700

    adding bbh.rb, to facilitate cross-platform builds .. rake stuff to make building boo projects easier

---
+++ bbh.rb
@@ -0,0 +1,90 @@
+require 'find'
+require 'rexml/document'
+
+# BooBuildHelper (Bbh)
+class Bbh
+  def initialize()
+  end
+  
+  # public stuff
+  def self.findBooFilesIn(baseDir)
+    findFilesWithExtensionInBaseDir(baseDir, '.boo').join(' ')
+  end
+  
+  def self.referenceAssemblies(assemblies)
+    cleanAsms = []
+    assemblies.each do |path|
+      temp = convertToPlatformSeparator(path)
+      cleanAsms.push(temp)
+    end
+    "-r:"+cleanAsms.join(',') + ' '
+  end
+  
+  def self.dllTarget
+    '-target:library '
+  end
+
+  def self.exeTarget
+    '-target:exe '
+  end
+
+  def self.referenceDependenciesInMSBuild(path, isRelativePath)
+    path = convertToPlatformSeparator(path)
+    projFile = REXML::Document.new File.read(path)
+    dirName = File.dirname(path)
+    
+    asmNames = []
+    hintPaths = []
+    projFile.elements.each('Project/ItemGroup/Reference') do |reference|
+      asmNames.push reference.attributes['Include']
+      if reference.elements['HintPath'] != nil
+        reference.elements.each('HintPath') do |path|
+          if isRelativePath
+            hintPaths.push convertToPlatformSeparator(dirName + "/" + path.text)
+          else
+            hintPaths.push convertToPlatformSeparator(path.text)
+          end
+        end
+      else
+        hintPaths.push '' 
+      end
+    end
+    refs = []
+    asmNames.each_with_index do |asmName, i|
+      if hintPaths[i] == '' 
+        refs.push asmName
+      else
+        refs.push hintPaths[i]
+      end
+    end
+    referenceAssemblies(refs)
+  end
+  
+  def self.convertToPlatformSeparator(path)
+    path.gsub(/(\\|\/)/, File::SEPARATOR)
+  end
+  
+  # internal stuff
+  def self.findFilesWithExtensionInBaseDir(baseDir, extension)
+    arr = []
+    baseDir = convertToPlatformSeparator(baseDir)
+    exploreDirSearchForFilesAndStoreInList(baseDir, extension, arr)
+    arr.uniq
+  end
+
+  def self.exploreDirSearchForFilesAndStoreInList(dir, extension, arr)
+    Find.find(dir) do |path|
+      if path == dir
+        next
+      end
+      if FileTest.directory?(path)
+        exploreDirSearchForFilesAndStoreInList(path, extension, arr)
+      else
+        if File.extname(path).downcase == extension
+          arr.push(path)
+        end
+      end
+    end
+  end
+
+end
